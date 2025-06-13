4cce897b87ac0ef46c03930d4b36644e9a27c2a8
anoopkumargithub <anoopkumar7818@outlook.com>
2025-06-13 14:37:00 +0530

    fix: correct countdown loop condition in rocketLaunch.java

--- JavaControlFlow/rocketLaunch.java
+++ JavaControlFlow/rocketLaunch.java
@@ -6,7 +6,7 @@ public class rocketLaunch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int  countDown = sc.nextInt();
-        for(int i = countDown;i>=0;i--){
+        for(int i = countDown;i>0;i--){
             System.out.println(i);
         }
         sc.close();;

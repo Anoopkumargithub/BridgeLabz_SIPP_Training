40b3d1868520e5713faf208db4bd9ae313f0f183
anoopkumargithub <anoopkumar7818@outlook.com>
2025-07-09 14:57:53 +0530

    [Anoop kumar]  ADD: programs

---
+++ StackandQueue/stockSpan.java
@@ -0,0 +1,8 @@
+package StackandQueue;
+
+public class stockSpan {
+    public static void main(String[] args) {
+        int stocks[] = {100,80,60,70,60,75,85};
+        int[] span = new int[stocks.length];
+    }
+}

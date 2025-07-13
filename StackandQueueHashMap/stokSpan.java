e9a2992caff6c1156d6ac1f5c4c8e8e94b4d8afc
anoopkumargithub <anoopkumar7818@outlook.com>
2025-07-14 03:35:26 +0530

    [Anoop Kumar] ADD: stock span prblm

--- StackandQueueHashMap/stokSpan.java
+++ StackandQueueHashMap/stokSpan.java
@@ -8,21 +8,15 @@ public class stokSpan {
         int[] stock = {100, 80, 60, 70, 60, 75, 85};
         ArrayList<Integer> span = new ArrayList<>();
         Stack<Integer> st = new Stack<>();
-
-        
-        
-        for (int i = 0; i < stock.length; i++) {
-            int count = 1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (stock[j] <= stock[i]) {
-                    count++;
-                } else {
-                    break;
-                }
+        st.push(0);
+        span.add(1);
+        for (int i = 1; i < stock.length; i++) {
+            while (!st.isEmpty() && stock[st.peek()] <= stock[i]) {
+                st.pop();
             }
-            span.add(count);
+            span.add(st.isEmpty() ? i + 1 : i - st.peek());
+            st.push(i);
         }
-
         System.out.println(span.toString());
     }
 }

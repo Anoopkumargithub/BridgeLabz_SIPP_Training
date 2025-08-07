c6a10d75d059fd49658df5db9fafb1e64e71c6d9
anoopkumargithub <anoopkumar7818@outlook.com>
2025-08-07 14:22:08 +0530

    [Anoop Kumar] Add: Regex Example

---
+++ regex/extractEmail.java
@@ -0,0 +1,19 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class extractEmail {
+    public static void main(String[] args) {
+        String sentence = "Contact us at support@example.com and info@company.org";
+        String regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+         String[] words = sentence.trim().split("\\s+");
+         for (String word : words) {
+            Matcher matcher = pattern.matcher(word);
+            if(matcher.matches()){
+                System.out.println(word);
+            }
+        }
+    }
+}

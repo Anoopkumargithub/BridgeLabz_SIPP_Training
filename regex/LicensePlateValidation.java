72be030ac3943dc611121ce35eb51e40f16275d5
anoopkumargithub <anoopkumar7818@outlook.com>
2025-08-07 14:13:45 +0530

    [AnoopKumar] Add: License plate

--- regex/LicensePlateValidation.java
+++ regex/LicensePlateValidation.java
@@ -7,10 +7,10 @@ public class LicensePlateValidation {
     public static void main(String[] args) {
         String regex = "^[A-Z]{2}+[0-9]{4}$";
         Pattern pattern = Pattern.compile(regex);
-         String[] emails = {"AB1234", "1234AB", "us1234"};
-         for (String email : emails) {
-            Matcher matcher = pattern.matcher(email);
-            System.out.println(email + " is valid: " + matcher.matches());
+         String[] plates = {"AB1234", "1234AB", "us1234"};
+         for (String plate : plates) {
+            Matcher matcher = pattern.matcher(plate);
+            System.out.println(plate + " is valid: " + matcher.matches());
         }
     }
 }

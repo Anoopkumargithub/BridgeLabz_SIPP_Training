24f5fc8b6af60ffb98ff4566a67027c0e0320812
anoopkumargithub <anoopkumar7818@outlook.com>
2025-08-07 14:13:59 +0530

    [AnoopKumar] Add: Usernames

--- regex/UsernameValidation.java
+++ regex/UsernameValidation.java
@@ -7,10 +7,10 @@ public class UsernameValidation {
     public static void main(String[] args) {
         String regex = "^[a-zA-Z]+[a-zA-Z0-9_.-]{5,15}$";
         Pattern pattern = Pattern.compile(regex);
-         String[] emails = {"user_123", "123user", "us"};
-         for (String email : emails) {
-            Matcher matcher = pattern.matcher(email);
-            System.out.println(email + " is valid: " + matcher.matches());
+         String[] usernames = {"user_123", "123user", "us"};
+         for (String username : usernames) {
+            Matcher matcher = pattern.matcher(username);
+            System.out.println(username + " is valid: " + matcher.matches());
         }
     }
 }
